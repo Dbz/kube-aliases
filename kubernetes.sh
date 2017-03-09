a84533fa44979ad7f29a0338492477e7845bec8b
daniel.burt <daniel.burt@workday.com>
2017-03-09 11:31:55 -0800

    Added kubernetes aliases and bash functions

---
+++ kubernetes.sh
@@ -0,0 +1,29 @@
+alias kube='kubectl'
+alias kc='kubectl'
+alias kgp='kubectl get pods'
+alias kgd='kubectl get deployments'
+alias kgs='kubectl get services'
+alias kgi='kubectl get ingress'
+alias kgn='kubectl get namespace'
+alias kcc='kubectl config get-contexts'
+alias kdp='kubectl delete pod'
+alias kdd='kubectl delete deployment'
+alias kdf='kubectl delete -f'
+alias klf='kubectl logs -f'
+alias kaf='kubectl apply -f'
+alias kra='krender; kapply'
+
+ksc () {
+  kubectl config set-context "${2:-$(kubectl config current-context)}" --namespace "${1:-syman}"
+}
+
+krender () {
+  fasd_cd scylla
+  ruby lib/kube/render_resources.rb config/ . kubernetes "${1:-2017.04.017}"
+}
+
+kapply () {
+  fasd_cd scylla
+  kc apply -f k8s_resources/dev/kubernetes/syman_components/ -R --namespace="${1:-syman}"
+}
+
