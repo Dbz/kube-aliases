f0419419440612ca9e6985025882d24fa1f57bf1
Patrick Messina <patrick@messinamail.com>
2019-12-18 17:24:04 -0600

    kube-aliases now uses generated aliases

--- kube-aliases.plugin.zsh
+++ kube-aliases.plugin.zsh
@@ -1,7 +1,20 @@
 export KALIAS=$(dirname $0)
 export KRESOURCES=$(dirname $0)/docs/resources
 export KSHELL=$(basename $SHELL)
+export KALIAS_YAML_PATH=$(dirname $0)/aliases.yaml
 
 bin="$(dirname $0)/bin"
 export PATH=${PATH}:${bin}
-source $(dirname $0)/old.zsh
+# source $(dirname $0)/old.zsh
+
+source <(kubectl completion $KSHELL)
+
+# If aliases file exists source it
+generated_alias_file=$(dirname $0)/aliases
+if [ -f $generated_alias_file ]; then
+  source ${generated_alias_file}
+fi
+
+alias k="kubectl"
+alias kctx="kubectx"
+alias kns="kubens"
