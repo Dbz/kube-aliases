f0ae103adea5918512b50ca546f018eed18f987b
Patrick Messina <patrick_messina@eogresources.com>
2021-08-19 16:01:35 -0500

    refactor to be more useable

--- generate_aliases/pkg/generate/generate.go
+++ generate_aliases/pkg/generate/generate.go
@@ -1,4 +1,4 @@
-package main
+package generate
 
 import (
 	"fmt"
@@ -10,91 +10,78 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-// filepath to generated aliases
-var aliasPath string
-
-// filepath to yaml custom alias file
-var aliasYamlPath string
-
 // CMD contains a Kubernetes Command and its short aliases
 type CMD struct {
 	CMD   string `yaml:"cmd"`
 	Short string `yaml:"short"`
+	// TODO: Added suffix and prefix commands
+	// Prefix string `yaml:"prefix,omitempty"`
+	// Suffix string `yaml:"suffix,omitempty"`
 }
 
-// CMDs is just a map of CMDs
 type CMDs map[string]CMD
 
-// Resource in Kubernetes , additional CMDs allows for resources specific cmds
+// Resources in Kubernetes and specifies its short form
 type Resource struct {
-	Short         string `yaml:"short"`
-	AdditonalCMDs []CMD  `yaml:"add"`
+	Short string `yaml:"short"`
+	// Additional commands that do not need to be generated for all resources
+	AdditonalCMDs []CMD `yaml:"add"`
 }
 
 // Resources is a collection of resources
 type Resources map[string]Resource
 
 // Aliases is the collection of Resources and CMDs, and should contain
-// the entire read in file
+// is used to read in the given yaml file
 type Aliases struct {
 	Resources Resources `yaml:"resources"`
 	CMDs      CMDs      `yaml:"cmds"`
 }
 
-func init() {
-
-	var kaliasExists bool
-	aliasPath, kaliasExists = os.LookupEnv("KALIAS")
-	if !kaliasExists {
-		log.Fatalf("kube-aliases is not installed, or is not installed properly.\n")
-	}
-	aliasPath += "/aliases"
-
-	aliasYamlPath, kaliasExists = os.LookupEnv("KALIAS_YAML_PATH")
-	if !kaliasExists {
-		log.Fatalf("kube-aliases is not installed, or is not installed properly.\n")
-	}
-
-}
-
-func main() {
-
-	yamlFile, err := ioutil.ReadFile(aliasYamlPath)
+func Generate(filePath, targetPath string) {
+	file, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		log.Printf("yamlFile.Get err   #%v ", err)
+		log.Fatalf("error reading file %s with error %s",
+			filePath, err)
 	}
 
 	var aliases Aliases
-	err = yaml.Unmarshal(yamlFile, &aliases)
+	err = yaml.Unmarshal(file, &aliases)
 	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
+		log.Fatalf("error unmarshaling file: %v", err)
 	}
 
-	file, err := os.Create(aliasPath)
+	aliasFile, err := os.Create(targetPath)
 	if err != nil {
 		log.Fatalf("Failed to create alias file: %v", err)
 	}
-	defer file.Close()
+	defer aliasFile.Close()
 
 	for r := range aliases.Resources {
+
+		// Generate Commands
 		for c := range aliases.CMDs {
+
 			s := fmt.Sprintf("alias k%v%v='kubectl %v %v'\n",
 				aliases.CMDs[c].Short, aliases.Resources[r].Short,
 				aliases.CMDs[c].CMD, r)
-			_, err = io.WriteString(file, s)
+
+			_, err = io.WriteString(aliasFile, s)
 			if err != nil {
 				log.Printf("Warning: could not write alias: %v\n", s)
 			}
 		}
+
 		for _, v := range aliases.Resources[r].AdditonalCMDs {
 			s := fmt.Sprintf("alias k%v='kubectl %v '\n",
 				v.Short,
 				v.CMD)
-			_, err = io.WriteString(file, s)
+			_, err = io.WriteString(aliasFile, s)
 			if err != nil {
 				log.Printf("Warning: could not write alias: %v\n", s)
 			}
 		}
+
 	}
 
 }
