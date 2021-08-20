e83aa4a708543bb4d690eba26e7c46bbd80d23bf
Patrick Messina <patrick_messina@eogresources.com>
2021-08-20 10:54:19 -0500

    no longer need a small wrapper on file functions as kubectl now works on directories

--- kube-aliases.plugin.zsh
+++ kube-aliases.plugin.zsh
@@ -9,12 +9,20 @@ export PATH=${PATH}:${bin}
 
 source <(kubectl completion $KSHELL)
 
+# TODO: Probably can be removed
 # If aliases file exists source it
 generated_alias_file=$(dirname $0)/aliases
 if [ -f $generated_alias_file ]; then
   source ${generated_alias_file}
 fi
 
+# Additional aliases not covered by generation
 alias k="kubectl"
 alias kctx="kubectx"
 alias kns="kubens"
+
+# TODO: Should be auto generated but they dont need a script anymore
+alias kcf="kubectl create -f"
+alias kaf="kubectl apply -f"
+alias kgf="kubectl get -f"
+alias kdelf="kubectl delete -f"
